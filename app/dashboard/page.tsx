306944d6e220c384173d465f6b83a3acbe91af0c
JD Sir <44726606+shihan84@users.noreply.github.com>
2026-09-17 19:25:30 +0530

    Show permanent VCPR levels on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,11 @@
 import MarketDashboard from '../../components/MarketDashboard';
+import VcprLevelsPanel from '../../components/VcprLevelsPanel';
 
 export default function DashboardPage() {
-  return <MarketDashboard />;
+  return (
+    <>
+      <MarketDashboard />
+      <VcprLevelsPanel />
+    </>
+  );
 }
