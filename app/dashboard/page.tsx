6aa711858921088119bc86f79ad27fa403f0aeec
JD Sir <44726606+shihan84@users.noreply.github.com>
2026-09-17 22:53:49 +0530

    Show VCPR reaction analytics on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import MarketDashboard from '../../components/MarketDashboard';
 import VcprLevelsPanel from '../../components/VcprLevelsPanel';
+import VcprReactionAnalytics from '../../components/VcprReactionAnalytics';
 
 export default function DashboardPage() {
   return (
     <>
       <MarketDashboard />
       <VcprLevelsPanel />
+      <VcprReactionAnalytics />
     </>
   );
 }
